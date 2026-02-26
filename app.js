89803fd0fb0282d52dcb0ad66c5106ac528357f1
kamillycampos453-maker <you@example.com>
2026-02-25 21:59:19 -0300

    inicialização do server

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log ("teste")
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler (request, reply) {
+const users = [
+  
+    {nome: "kamilly", idade: 19} ,
+    {nome: "gabriel", idade: 22}
+    
+  
+]
+return users;
+})
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
+
